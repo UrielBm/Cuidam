3505c2ae1d7e8aa572b5f13a0fcd0d4e95ccf315
Uriel Benítez <urielbenitez96@hotmail.com>
2020-05-18 18:53:32 -0500

    Subiendo Cuidam

--- src/Componentes/Navegacion.jsx
+++ src/Componentes/Navegacion.jsx
@@ -10,8 +10,8 @@ class Navegacion extends Component {
                     <nav>
                         <ul>
                             <li><a  href="/">Inicio</a></li>
-                            <li><a  href="Mision_y_Vision">Misión y Visión</a></li>
-                            <li><a  href="Nosotros">Nosotros</a></li>
+                            <li><a  href="/Mision_y_Vision">Misión y Visión</a></li>
+                            <li><a  href="/Nosotros">Nosotros</a></li>
                             <li><a>Redes Sociales: &nbsp; </a><a href="https://www.facebook.com/108756747387381/posts/108817624047960/?sfnsn=scwspmo&extid=okowd6aQpbTSTxdY" target="blank"><FacebookFilled  className="IconosNav"/></a> &nbsp; <a href="https://twitter.com/cuidam"target="blank" ><TwitterOutlined className="IconosNav" /></a> &nbsp;<a href="https://api.whatsapp.com/send?phone=527712003337&text=¡Hola!,%20Me%20podrian%20dar%20infomes" target="blank"><WhatsAppOutlined className="IconosNav" /></a></li>
                         </ul>
                     </nav>
